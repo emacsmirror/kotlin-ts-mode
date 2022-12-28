9b2a63d2ba067855883c01c23129af873c31637f
Alex Figl-Brick <alex@alexbrick.me>
2022-12-28 21:59:32 +0100

    Licensing under GPL3

--- kotlin-ts-mode.el
+++ kotlin-ts-mode.el
@@ -1,7 +1,24 @@
 ;;; kotlin-ts-mode.el --- A mode for editing Kotlin files based on tree-sitter  -*- lexical-binding: t; -*-
 
+;; Copyright 2022 Alex Figl-Brick
+
+
 ;; Author: Alex Figl-Brick <alex@alexbrick.me>
 ;; Package-Requires: ((emacs "29"))
+
+;; This program is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see
+;; <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
